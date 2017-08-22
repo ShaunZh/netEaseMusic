af923ab79ac7850e37a0cb8217ec3e196ddc38a2
hexonZhang <308826842@qq.com>
2017-08-22 16:17:10 +0800

    1. 添加svg的播放暂停icon 2. 添加播放和暂停功能

--- js/song.js
+++ js/song.js
@@ -10,7 +10,6 @@ function getLyric() {
   $.get('../data/lyric.json').then(function (object) {
     let lyric = object.lrc.lyric;
     let array = lyric.split('\n');
-    console.log(array);
     let regex = /^\[(.+)\](.+)/;
     array = array.map(function (string, index) {
       let matches = string.match(regex);
@@ -24,19 +23,25 @@ function getLyric() {
       let $p = $('<p/>');
       $p.attr('data-time', object.time).text(object.words);
       $lines.append($p);
-      console.log($p);
     })
 
-  })
+  });
   let audio = document.createElement('audio');
   audio.src ="http://m10.music.126.net/20170822150457/e215e0292b3255ecd0ab4985a434bd25/ymusic/e1db/9abe/8b0a/b5deceec956392e4eae421dac085a564.mp3";
   audio.oncanplay = function () {
     audio.play();
     $('.disc-container').addClass('playing');
-    
-  }
-}
-//
-// http://m10.music.126.net/20170822150457/e215e0292b3255ecd0ab4985a434bd25/ymusic/e1db/9abe/8b0a/b5deceec956392e4eae421dac085a564.mp3
+  };
 
+  $('.icon-pause').on('click', function () {
+    console.log('pause');
+    audio.pause();
+    $('.disc-container').removeClass('playing');
+  });
+
+  $('.icon-play').on('click', function () {
+    audio.play();
+    $('.disc-container').addClass('playing');
+  });
+}
 getLyric();
