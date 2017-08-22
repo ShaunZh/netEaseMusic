3997b74ba8ca3db9575487303c267101b6e847e3
hexonZhang <308826842@qq.com>
2017-08-22 15:32:40 +0800

    将文件分类、完成播放功能

---
+++ js/song.js
@@ -0,0 +1,42 @@
+// $(function() {
+//   $.get('./lyric.json').then(function (object) {
+//     let {lyric} = object;
+//     console.log(lyric.split('\n'));
+//   })
+// })
+
+
+function getLyric() {
+  $.get('../data/lyric.json').then(function (object) {
+    let lyric = object.lrc.lyric;
+    let array = lyric.split('\n');
+    console.log(array);
+    let regex = /^\[(.+)\](.+)/;
+    array = array.map(function (string, index) {
+      let matches = string.match(regex);
+      if (matches) {
+        return {time: matches[1], words: matches[2]}
+      }
+    });
+    let $lines = $('.lines')
+    array.map(function (object) {
+      if (!object) {return}
+      let $p = $('<p/>');
+      $p.attr('data-time', object.time).text(object.words);
+      $lines.append($p);
+      console.log($p);
+    })
+
+  })
+  let audio = document.createElement('audio');
+  audio.src ="http://m10.music.126.net/20170822150457/e215e0292b3255ecd0ab4985a434bd25/ymusic/e1db/9abe/8b0a/b5deceec956392e4eae421dac085a564.mp3";
+  audio.oncanplay = function () {
+    audio.play();
+    $('.disc-container').addClass('playing');
+    
+  }
+}
+//
+// http://m10.music.126.net/20170822150457/e215e0292b3255ecd0ab4985a434bd25/ymusic/e1db/9abe/8b0a/b5deceec956392e4eae421dac085a564.mp3
+
+getLyric();
