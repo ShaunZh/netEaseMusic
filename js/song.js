7b408788737d22cb1b272dd1761023fbebf3cecc
hexonZhang <308826842@qq.com>
2017-08-23 13:41:23 +0800

    1. 修改播放页面点击停止时，旋转的图片会回到起始位置的bug 2. 完成从首页跳转到播放页面的功能 3. 在播放页面将歌与歌词匹配

--- js/song.js
+++ js/song.js
@@ -5,37 +5,10 @@
 //   })
 // })
 
-let audioUrl = "//oqvj0lqwc.bkt.clouddn.com/%E5%A4%A7%E7%BA%A6%E5%9C%A8%E5%86%AC%E5%AD%A3.mp3";
 
-function getLyric() {
-  $.get('../data/lyric.json').then(function (object) {
-    let lyric = object.lrc.lyric;
-    let array = lyric.split('\n');
-    let regex = /^\[(.+)\](.+)/;
-    array = array.map(function (string, index) {
-      let matches = string.match(regex);
-      if (matches) {
-        return {time: matches[1], words: matches[2]}
-      }
-    });
-    let $lines = $('.lines')
-    array.map(function (object) {
-      if (!object) {return}
-      let $p = $('<p/>');
-      $p.attr('data-time', object.time).text(object.words);
-      $lines.append($p);
-    })
-
-  });
-  let audio = document.createElement('audio');
-  audio.src = audioUrl;
-  audio.oncanplay = function () {
-    audio.play();
-    $('.disc-container').addClass('playing');
-  };
-
+function addEvent() {
+  let audio = document.querySelector('.disc-container audio');
   $('.icon-pause').on('click', function () {
-    console.log('pause');
     audio.pause();
     $('.disc-container').removeClass('playing');
   });
@@ -45,4 +18,75 @@ function getLyric() {
     $('.disc-container').addClass('playing');
   });
 }
-getLyric();
+
+function songDeal(id, successFn, errorFn) {
+  $.get('../songs.json').then(function(response) {
+    let songs = response;
+    let song = songs.filter((s) => parseInt(s.id) === id);
+    console.log('song');
+    successFn && successFn.apply(null, song);
+    addEvent();
+  });
+}
+
+
+function getSongId() {
+  id = parseInt(location.href.match(/id=([^&])*/)[1]);
+  return id;
+}
+
+function getLyric(song) {
+  if (song.hasOwnProperty('lrc') && song.lrc.hasOwnProperty('lyric')) {
+    let lyric = song.lrc.lyric;
+    let array = lyric.split('\n');
+    let regex = /^\[(.+)\](.+)/;
+    array = array.map(function (string, index) {
+      let matches = string.match(regex);
+      if (matches) {
+        return {time: matches[1], words: matches[2]}
+      }
+    });
+    return array
+  }
+  return "获取歌词失败";
+}
+
+function dispSongName(song) {
+  if (typeof song.name === 'string') {
+    $('.song-description>h1').text(song.name);
+  }
+}
+
+function dispLyric(lyric) {
+  let $lines = $('.lines');
+  if (typeof lyric === 'string') {
+    let $p = $('<p/>');
+    $p.text(lyric);
+    $lines.append($p);
+    return
+  }
+  lyric.map(function (object) {
+    if (!object) {return}
+    let $p = $('<p/>');
+    $p.attr('data-time', object.time).text(object.words);
+    $lines.append($p);
+  })
+}
+
+function playSong(song) {
+  let audio = document.querySelector('.disc-container audio');
+  audio.src = song.url;
+  console.log(audio);
+  audio.oncanplay = function () {
+    audio.play();
+    $('.disc-container').addClass('playing');
+    dispSongName(song);
+    let lyric = getLyric(song);
+    dispLyric(lyric);
+  };
+}
+
+$(function(){
+  let songId = getSongId();
+  songDeal(songId, playSong);
+});
