738483b425c126391c0e557d5e0e18850c98fa74
hexonZhang <308826842@qq.com>
2017-08-23 11:29:29 +0800

    完成进入首页就从json数据中获取音乐信息

---
+++ js/index.js
@@ -0,0 +1,53 @@
+/**
+ * @Author: Hexon
+ * @Date: 2017/8/23 11:00
+ * @Last Modified by: Hexon
+ * @Last Modified time: 2017/8/23 11:00
+ */ 
+
+$(function() {
+  $.get('../songs.json').then(function(response){
+    let $latestMusic = $('.latestMusic .lists');
+    let $loadingIcon = $('.latestMusic .loadingIcon');
+    console.log($latestMusic);
+    console.log(response);
+    let items = response;
+    items.forEach((item) => {
+      let $li = $(`
+      <li>
+        <a href="./pages/song.html?id=${item.id}" class="play-circle">
+          <h3>${item.name}</h3>
+          <p>
+          <svg class="sq">
+             <use xlink:href="#icon-sq"></use>
+          </svg>
+          演唱者-专辑
+          </p>
+          <svg>
+            <use xlink:href="#icon-play-circle"></use>
+          </svg>
+        </a>
+      </li>
+        `);
+      $latestMusic.append($li);
+    });
+    $loadingIcon.hide();
+
+  });
+  /*
+   <li>
+   <a href="./pages/song.html" class="play-circle">
+   <h3>歌曲名1</h3>
+   <p>
+   <svg class="sq">
+   <use xlink:href="#icon-sq"></use>
+   </svg>
+   演唱者1-专辑1
+   </p>
+   <svg>
+   <use xlink:href="#icon-play-circle"></use>
+   </svg>
+   </a></li>
+   <li>
+  * */
+});
