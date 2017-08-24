90799b6fbf287f6d9eeabb470fd6d3d9616a14b4
hexonZhang <308826842@qq.com>
2017-08-24 20:20:04 +0800

    通过ajax获取歌单信息，并完成歌单介绍显示部分

--- js/musicSheet.js
+++ js/musicSheet.js
@@ -5,9 +5,42 @@
  * @Last Modified time: 2017/8/24 19:22
  */
 
-$(function(){
-  $.get('../musicSheet.json').then((response) => {
-    console.log(response);
+
+function dispSheetInfo(info) {
+  // header 部分
+  let num = parseInt(info.fans);
+  let fansNum = num < 100000 ? info.fans : ((num / 10000).toFixed(1) + '万');
+  $('.header > .sheetImgWrap > img').attr('src', info.sheetImg);
+  $('.header > .sheetImgWrap > .fans > .num').text(fansNum) ;
+  $('.header > .headerTitle > h3').text(info.title);
+  $('.header > .headerTitle > .author > span').text(info.author) ;
+  $('.header > .headerTitle > .author > img').attr('src',  info.authorPhoto) ;
+
+  let $briefInfo = $('.sheetIntro .birefIntro');
+  // 简介部分
+  info.flag.forEach((item) => {
+    let $flag = $(`
+        <span class="flag">${item}</span>
+    `)
+    $briefInfo.append($flag);
+  });
+  // 歌单介绍
+  let $intro = $('.sheetIntro .intro');
+  let introArr = info.intro.split('\n');
+  introArr.forEach((item) => {
+    if (item.length > 0) {
+      let $p = $(`
+        <p>${item}</p>
+    `)
+      $intro.append($p);
+    }
   })
 
+}
+$(function(){
+  console.log('hhh')
+  $.get('./../musicSheet.json').then(function(response) {
+    dispSheetInfo(response.musicInfo)
+
+  })
 });
